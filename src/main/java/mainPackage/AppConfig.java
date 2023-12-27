42a2d064c7f2c4b415687a068fce02b8bae066f6
Gopi V <gopi@lp.local>
2023-12-27 16:13:46 +0530

    false

--- src/main/java/mainPackage/AppConfig.java
+++ src/main/java/mainPackage/AppConfig.java
@@ -5,7 +5,7 @@ public class AppConfig {
 	
 	
 
-	public static boolean saveButtonOnAndOff= true;
+	public static boolean saveButtonOnAndOff= false;
 	public static String username= "mds0418@gmail.com";
 	public static String password="KRm#V39fecMDGg#";
 	public static String URL="https://app.propertyware.com/pw/login.jsp";
